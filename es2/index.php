1fa1f0726329f7658305daa70445b6592bcb2ab3
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-26 12:46:27 +0200

    Creazione form

---
+++ es2/index.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<form action="./index.php" method="get">
+
+<label for="nome">Nome: </label>
+<input type="text" name="nome" id="nome">
+
+<label for="eta">Eta: </label>
+<input type="number" name="eta" id="eta">
+
+<label for="email">Email: </label>
+<input type="email" name="email" id="email">
+<input type="submit" value="Convalida">
+
+
+</form>
+
+
+</body>
+</html>
