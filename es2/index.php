3dccd71ae50a068ff7514748ba077df7f087f333
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-26 12:58:18 +0200

    uso funzione per convertire il valore inserito nell' input[type="eta"] in un numero

--- es2/index.php
+++ es2/index.php
@@ -1,10 +1,11 @@
 <?php
 
 
+$eta =  intval($_GET["eta"]);
 
 
-
-
+isset($_GET["eta"]);
+echo $eta;
 
 
 
@@ -16,7 +17,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Es2</title>
 </head>
 <body>
     
@@ -27,7 +28,7 @@
 <input type="text" name="nome" id="nome">
 
 <label for="eta">Eta: </label>
-<input type="number" name="eta" id="eta">
+<input type="text" name="eta" id="eta">
 
 <label for="email">Email: </label>
 <input type="email" name="email" id="email">
