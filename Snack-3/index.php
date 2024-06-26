ed6c203bf93d2f7704b9daeea1329a99c0fdd870
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-26 21:43:13 +0200

    Creare array per snack-3

---
+++ Snack-3/index.php
@@ -0,0 +1,54 @@
+<?php
+
+$post = [
+
+    '10/11/2024' => [
+
+        [
+            'title' => 'post 1',
+            'author' => 'Luca Rubino',
+            'text' => 'text post 1',
+        ],
+        [
+            'title' => 'post 2',
+            'author' => 'Luca Rubino',
+            'text' => 'text post 2',
+        ],
+
+    ],
+
+    '09/12/2024' => [
+
+        [
+            'title' => 'post 3',
+            'author' => 'Luca Rubino',
+            'text' => 'text post 3',
+        ],
+
+    ],
+
+    '25/10/2024' => [
+
+        [
+            'title' => 'post 4',
+            'author' => 'Luca Rubino',
+            'text' => 'text post 4',
+        ],
+        [
+            'title' => 'post 5',
+            'author' => 'Luca Rubino',
+            'text' => 'text post 5',
+        ],
+        [
+            'title' => 'post 6',
+            'author' => 'Luca Rubino',
+            'text' => 'text post 6',
+        ],
+
+    ],
+
+];
+
+// echo $post;
+
+?>
