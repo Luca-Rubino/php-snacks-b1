43704fb52418bdc692c17ae94b5a2c0fffb0b80d
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-26 12:21:27 +0200

    Svolgimento esercizio (anche se non stampa sul dom)

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+$array = [
+    "chiave0" => [
+        '0a','0b','0c','0d',
+    ],
+    
+    "chiave1" =>[
+        '1a','1b','1c','1d'
+    ],
+    
+    "chiave2" =>[
+        '2a','2b','2c','2d'
+    ]
+];
+
+echo $array["chiave0"];
+echo $array["chiave1"];
+echo $array["chiave2"];
+
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php-snacks-B1</title>
+</head>
+<body>
+    
+</body>
+</html>
