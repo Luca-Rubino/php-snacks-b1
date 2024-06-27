59108e197450b4fc592a033002e95d6b5e97e1af
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-27 12:52:32 +0200

    Creare array animali

--- Snack-8/animals.php
+++ Snack-8/animals.php
@@ -2,9 +2,54 @@
 
 $animali = [
     [
-        'nome' => '',
+        'nome' => 'elefante',
+        'specie' => 'asiatico',
+        'classe' => '9',
+    ],
+    [
+        'nome' => 'tigre',
+        'specie' => 'grandi felini',
+        'classe' => '10',
+    ],
+    [
+        'nome' => 'pantera',
+        'specie' => 'grandi felini',
+        'classe' => '9',
+    ],
+    [
+        'nome' => 'iena',
+        'specie' => 'il protele',
+        'classe' => '8',
+    ],
+    [
+        'nome' => 'riccio',
         'specie' => '',
-        'classe' => '',
+        'classe' => '2',
+    ],
+    [
+        'nome' => 'cane',
+        'specie' => 'canidi',
+        'classe' => '3',
+    ],
+    [
+        'nome' => 'gatto',
+        'specie' => 'felini',
+        'classe' => '3',
+    ],
+    [
+        'nome' => 'lemure',
+        'specie' => 'sifaka',
+        'classe' => '7',
+    ],
+    [
+        'nome' => 'scimmia',
+        'specie' => 'bonobo',
+        'classe' => '6',
+    ],
+    [
+        'nome' => 'armadillo',
+        'specie' => 'a nove fasce',
+        'classe' => '5',
     ],
 ];
 
