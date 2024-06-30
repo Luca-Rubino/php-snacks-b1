456bae1bca437c32e484c5ec8a0f7c6d1b1c0644
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-30 23:10:08 +0200

    Utilizzo di if else per stampare la riuscita o meno 'dell'accesso' a seconda delle condizioni della traccia

--- Snack-2/index.php
+++ Snack-2/index.php
@@ -1,5 +1,10 @@
 <?php
 
+if (strlen($_GET["nome"]) > 2 && str_contains($_GET["email"], ".") && str_contains($_GET["email"], "@") && is_numeric($_GET["eta"])) {
+    echo "Accesso riuscito";
+} else {
+    echo "Accesso negato";
+}
 
 ?>
 
@@ -12,22 +17,20 @@
 </head>
 <body>
     
+<form action="index.php" method="get">
 
-<form action="./index.php" method="get">
+    <label for="nome">Nome: </label>
+    <input type="text" name="nome" id="nome">
 
-<label for="nome">Nome: </label>
-<input type="text" name="nome" id="nome">
+    <label for="eta">Eta: </label>
+    <input type="text" name="eta" id="eta">
 
-<label for="eta">Eta: </label>
-<input type="text" name="eta" id="eta">
-
-<label for="email">Email: </label>
-<input type="email" name="email" id="email">
-<input type="submit" value="Convalida">
-
+    <label for="email">Email: </label>
+    <input type="email" name="email" id="email">
+    
+    <input type="submit" value="Convalida">
 
 </form>
-
 
 </body>
 </html>
